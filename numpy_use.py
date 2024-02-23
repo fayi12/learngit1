5d46e3494471a8273bc2dc75859ccc15c29daef8
fayi12 <95390951+fayi12@users.noreply.github.com>
2024-02-23 11:44:08 +0800

    第一次使用长传
    上传

---
+++ numpy_use.py
@@ -0,0 +1,12 @@
+
+import matplotlib
+import matplotlib.font_manager
+
+mf_fn = sorted(matplotlib.font_manager.findSystemFonts(fontpaths='C:\\Windows\\Fonts', fontext='ttf'))
+list_font = []
+print("可用的系统字体：", len(mf_fn))
+for i in mf_fn[0:365]:
+    list_font.append(i)
+
+
+print(list_font)
